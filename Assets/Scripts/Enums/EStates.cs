1ccc2cbddfc9eae6c240e52ca4422b849703726e
cstrauch01@gmail.com <cstrauch01@gmail.com>
2024-01-17 15:45:10 -0700

    Set up app state machine

---
+++ Assets/Scripts/Enums/EStates.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum States
+{
+    Start,
+    Draw,
+    Animate,
+    Edit,
+    Save
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,68 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public States currentState;
+    public GameObject UIStart;
+    public GameObject UIDraw;
+    public GameObject UIAnimate;
+    public GameObject UIEdit;
+    public GameObject UISave;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentState = States.Start;
+        ChangeToStart();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void ChangeToStart()
+    {
+        UIStart.SetActive(true);
+        UIDraw.SetActive(false);
+        UIAnimate.SetActive(false);
+        UIEdit.SetActive(false);
+        UISave.SetActive(false);
+    }
+
+    public void ChangeToDraw()
+    {
+        UIStart.SetActive(false);
+        UIDraw.SetActive(true);
+        UIAnimate.SetActive(false);
+        UIEdit.SetActive(false);
+        UISave.SetActive(false);
+    }
+
+    public void ChangeToAnimate()
+    {
+        UIStart.SetActive(false);
+        UIDraw.SetActive(false);
+        UIAnimate.SetActive(true);
+        UIEdit.SetActive(false);
+        UISave.SetActive(false);
+    }
+
+    public void ChangeToEdit()
+    {
+        UIStart.SetActive(false);
+        UIDraw.SetActive(false);
+        UIAnimate.SetActive(false);
+        UIEdit.SetActive(true);
+        UISave.SetActive(false);
+    }
+
+    public void ChangeToSave()
+    {
+        UIStart.SetActive(false);
+        UIDraw.SetActive(false);
+        UIAnimate.SetActive(false);
+        UIEdit.SetActive(false);
+        UISave.SetActive(true);
     }
 }
