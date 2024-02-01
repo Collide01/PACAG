29516492596cf977d39ab38e85b34df2a382bfa5
cstrauch01@gmail.com <cstrauch01@gmail.com>
2024-01-31 17:00:42 -0700

    Making progress on draw screen

---
+++ Assets/Scripts/DrawingMode/CursorHighlight.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class CursorHighlight : MonoBehaviour
+{
+    [SerializeField] DrawingManager drawingManager;
+    [SerializeField] Tilemap tilemap;
+    [SerializeField] Tile tile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tilemap.ClearAllTiles();
+
+        Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3Int position = tilemap.WorldToCell(worldPoint);
+
+        // Set the tile for color creation.
+        tilemap.SetTile(position, tile);
+
+        // Flag the tile, inidicating that it can change color.
+        // By default it's set to "Lock Color".
+        tilemap.SetTileFlags(position, TileFlags.None);
+
+        // Set the color.
+        tilemap.SetColor(position, Color.white);
+    }
+}
